74c5ec39755e2fa71a3b55300f47ae68a6a8ccc0
trung-dev <trungezeal115@gmail.com>
2024-04-05 15:24:45 +0700

    update controller

--- src/controllers/saleManager.controller.js
+++ src/controllers/saleManager.controller.js
@@ -1,95 +1,120 @@
 require("dotenv").config();
+const { STATUS, MESSAGE, ERROR_MESSAGE } = require("../utils/constant");
+
 const db = require("../models");
 const WhitelistManager = db.whitelistManager;
 
 exports.addWhitelist = async (req, res) => {
-  const { poolType, buyer, amount, price } = req.body;
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
+  try {
+    const { poolType, buyer, amount, price } = req.body;
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
 
-  const whitelistOptions = {
-    poolType: poolType,
-    buyer: buyer,
-    amount: amount,
-    price: price,
-  };
+    const whitelistOptions = {
+      poolType: poolType,
+      buyer: buyer,
+      amount: amount,
+      price: price,
+    };
 
-  await WhitelistManager.create(whitelistOptions)
-    .then((data) => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot add whitelist. Maybe data was not found!`,
-        });
-      } else {
-        res.send({
-          message: "add whitelist successfully!",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({
-        message: "Could not added",
+    await WhitelistManager.create(whitelistOptions)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({
+            status: STATUS.FAILED,
+            message: ERROR_MESSAGE.CAN_NOT_UPDATE,
+          });
+        } else {
+          res.send({
+            status: STATUS.OK,
+            message: MESSAGE.SUCCESS,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send({ status: STATUS.FAILED, message: error.message });
       });
-    });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
+  }
 };
 
 exports.updateWhitelist = async (req, res) => {
-  const { poolType, buyer, amount, price } = req.body;
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    const { poolType, buyer, amount, price } = req.body;
 
-  const filter = {
-    poolType: poolType,
-    buyer: buyer,
-  };
+    const filter = {
+      poolType: poolType,
+      buyer: buyer,
+    };
 
-  const update = {
-    amount: amount,
-    price: price,
-  };
+    const update = {
+      amount: amount,
+      price: price,
+    };
 
-  await WhitelistManager.findOneAndUpdate(filter, update)
-    .then((data) => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot update whitelist. Maybe data was not found!`,
-        });
-      } else {
-        res.send({
-          message: "update whitelist successfully!",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({
-        message: "Could not updated",
+    await WhitelistManager.findOneAndUpdate(filter, update)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({
+            status: STATUS.FAILED,
+            message: ERROR_MESSAGE.CAN_NOT_UPDATE,
+          });
+        } else {
+          res.send({
+            status: STATUS.OK,
+            message: MESSAGE.SUCCESS,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send({ status: STATUS.FAILED, message: error.message });
       });
-    });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
+  }
 };
 
 exports.getWhitelist = async (req, res) => {
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
-  const { poolType, limit, offset } = req.body;
-  if (!limit) limit = 15;
-  if (!offset) offset = 0;
-  if (!poolType) {
-    return res.send({ status: "FAILED", message: "Invalid pool type" });
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    const { poolType, limit, offset } = req.body;
+    if (!limit) limit = 15;
+    if (!offset) offset = 0;
+    if (!poolType) {
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.INVALID_INPUT });
+    }
+
+    let data = await WhitelistManager.find({ poolType: poolType });
+
+    let total = data.length;
+
+    // pagination
+    data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
+
+    // format result
+    const dataTable = data.map((data) => ({
+      // poolType: data.poolType,
+      buyer: data.buyer,
+      amount: data.amount,
+      price: data.price,
+    }));
+
+    return res.send({ status: STATUS.OK, ret: dataTable, total: total });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
   }
-
-  let data = await WhitelistManager.find({ poolType: poolType });
-
-  let total = data.length;
-
-  // pagination
-  data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
-
-  // format result
-  const dataTable = data.map((data) => ({
-    // poolType: data.poolType,
-    buyer: data.buyer,
-    amount: data.amount,
-    price: data.price,
-  }));
-
-  return res.send({ status: "OK", ret: dataTable, total: total });
 };
--- src/controllers/stakingManager.controller.js
+++ src/controllers/stakingManager.controller.js
@@ -1,8 +1,4 @@
 require("dotenv").config();
-const db = require("../models");
-const StakeManager = db.stakeManager;
-const HistoryStaking = db.historyStaking;
-const ClaimEvent = db.claimEvent;
 const {
   getTotalPendingUnstakedByAccount,
   getRequestUnstakeTime,
@@ -10,178 +6,230 @@ const {
   getLimitUnstakeTime,
 } = require("../contracts/staking_contract_calls");
 let { convertTimeStampToNumber } = require("../utils/utils");
+const { STATUS, MESSAGE } = require("../utils/constant");
+
+const db = require("../models");
+const StakeManager = db.stakeManager;
+const HistoryStaking = db.historyStaking;
+const ClaimEvent = db.claimEvent;
 
 exports.updatePendingUnstake = async (req, res) => {
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
-  const { caller } = req.body;
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    const { caller } = req.body;
 
-  let limitTime = await getLimitUnstakeTime();
-  let total = await getTotalPendingUnstakedByAccount(caller);
-  let pendingUnstakeList = [];
+    let limitTime = await getLimitUnstakeTime();
+    let total = await getTotalPendingUnstakedByAccount(caller);
+    let pendingUnstakeList = [];
 
-  await StakeManager.deleteMany({ caller });
+    await StakeManager.deleteMany({ caller });
 
-  for (let i = 0; i < parseInt(total); i++) {
-    let [amount, time] = await Promise.all([
-      getPendingUnstakingAmount(caller, i),
-      getRequestUnstakeTime(caller, i),
-    ]);
+    for (let i = 0; i < parseInt(total); i++) {
+      let [amount, time] = await Promise.all([
+        getPendingUnstakingAmount(caller, i),
+        getRequestUnstakeTime(caller, i),
+      ]);
 
-    const pendingUnstakeInfo = {};
+      const pendingUnstakeInfo = {};
 
-    pendingUnstakeInfo.caller = caller;
-    pendingUnstakeInfo.callerIndex = i;
-    pendingUnstakeInfo.amount = amount.replace(/\,/g, "") / 10 ** 12;
-    pendingUnstakeInfo.time =
-      convertTimeStampToNumber(time) + convertTimeStampToNumber(limitTime);
+      pendingUnstakeInfo.caller = caller;
+      pendingUnstakeInfo.callerIndex = i;
+      pendingUnstakeInfo.amount = amount.replace(/\,/g, "") / 10 ** 12;
+      pendingUnstakeInfo.time =
+        convertTimeStampToNumber(time) + convertTimeStampToNumber(limitTime);
 
-    pendingUnstakeList.push(pendingUnstakeInfo);
-  }
+      pendingUnstakeList.push(pendingUnstakeInfo);
+    }
 
-  // console.log({ pendingUnstakeList });
+    // console.log({ pendingUnstakeList });
 
-  await StakeManager.insertMany(pendingUnstakeList)
-    .then((data) => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot updatePendingUnstake. Maybe data was not found!`,
+    await StakeManager.insertMany(pendingUnstakeList)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({
+            status: STATUS.FAILED,
+            message: ERROR_MESSAGE.CAN_NOT_UPDATE,
+          });
+        } else {
+          res.send({
+            status: STATUS.OK,
+            message: MESSAGE.SUCCESS,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send({
+          status: STATUS.FAILED,
+          message: ERROR_MESSAGE.CAN_NOT_UPDATE,
         });
-      } else {
-        res.send({
-          message: "updated successfully!",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({
-        message: "Could not update",
       });
-    });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
+  }
 };
 
 exports.getPendingUnstake = async (req, res) => {
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
-  let { caller, limit, offset, status } = req.body;
-  if (!limit) limit = 15;
-  if (!offset) offset = 0;
-  if (!status) status = 0;
-  if (!caller) {
-    return res.send({ status: "FAILED", message: "Invalid Address" });
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    let { caller, limit, offset, status } = req.body;
+    if (!limit) limit = 15;
+    if (!offset) offset = 0;
+    if (!status) status = 0;
+    if (!caller) {
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.INVALID_ADDRESS });
+    }
+
+    let data = await StakeManager.find({ caller: caller });
+
+    let total = data.length;
+    if (status == 1) {
+      data = data.filter((e) => {
+        return +new Date() < e.time;
+      });
+      total = data.length;
+    } else if (status == 2) {
+      data = data.filter((e) => {
+        return +new Date() >= e.time;
+      });
+      total = data.length;
+    }
+
+    // pagination
+    data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
+
+    // format result
+    const dataTable = data.map((data) => ({
+      index: data.callerIndex,
+      caller: data.caller,
+      amount: data.amount,
+      time: data.time,
+    }));
+
+    return res.send({ status: STATUS.OK, ret: dataTable, total: total });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
   }
-
-  let data = await StakeManager.find({ caller: caller });
-
-  let total = data.length;
-  if (status == 1) {
-    data = data.filter((e) => {
-      return +new Date() < e.time;
-    });
-    total = data.length;
-  } else if (status == 2) {
-    data = data.filter((e) => {
-      return +new Date() >= e.time;
-    });
-    total = data.length;
-  }
-
-  // pagination
-  data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
-
-  // format result
-  const dataTable = data.map((data) => ({
-    index: data.callerIndex,
-    caller: data.caller,
-    amount: data.amount,
-    time: data.time,
-  }));
-
-  return res.send({ status: "OK", ret: dataTable, total: total });
 };
 
 exports.updateHistoryStaking = async (req, res) => {
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
-  const { caller, amount, currentTime, status } = req.body;
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    const { caller, amount, currentTime, status } = req.body;
 
-  const historyOptions = {
-    caller: caller,
-    amount: amount,
-    currentTime: currentTime,
-    status: status,
-  };
+    const historyOptions = {
+      caller: caller,
+      amount: amount,
+      currentTime: currentTime,
+      status: status,
+    };
 
-  await HistoryStaking.create(historyOptions)
-    .then((data) => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot add history staking. Maybe data was not found!`,
+    await HistoryStaking.create(historyOptions)
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({
+            status: STATUS.FAILED,
+            message: ERROR_MESSAGE.CAN_NOT_UPDATE,
+          });
+        } else {
+          res.send({
+            status: STATUS.OK,
+            message: MESSAGE.SUCCESS,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send({
+          message: "Could not added",
         });
-      } else {
-        res.send({
-          message: "add history staking successfully!",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({
-        message: "Could not added",
       });
-    });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
+  }
 };
 
 exports.getHistoryStaking = async (req, res) => {
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
-  let { caller, limit, offset, status } = req.body;
-  if (!limit) limit = 15;
-  if (!offset) offset = 0;
-  if (!status) status = 0;
-  if (!caller) {
-    return res.send({ status: "FAILED", message: "Invalid Address" });
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    let { caller, limit, offset, status } = req.body;
+    if (!limit) limit = 15;
+    if (!offset) offset = 0;
+    if (!status) status = 0;
+    if (!caller) {
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.INVALID_ADDRESS });
+    }
+
+    let data = await HistoryStaking.find({ caller: caller }).sort({
+      currentTime: -1,
+    });
+
+    let total = data.length;
+
+    // pagination
+    data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
+
+    // format result
+    const dataTable = data.map((data) => ({
+      caller: data.caller,
+      amount: data.amount,
+      currentTime: data.currentTime,
+      status: data.status,
+    }));
+
+    return res.send({ status: STATUS.OK, ret: dataTable, total: total });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
   }
-
-  let data = await HistoryStaking.find({ caller: caller }).sort({
-    currentTime: -1,
-  });
-
-  let total = data.length;
-
-  // pagination
-  data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
-
-  // format result
-  const dataTable = data.map((data) => ({
-    caller: data.caller,
-    amount: data.amount,
-    currentTime: data.currentTime,
-    status: data.status,
-  }));
-
-  return res.send({ status: "OK", ret: dataTable, total: total });
 };
 
 exports.getRewardByCaller = async (req, res) => {
-  if (!req.body) return res.send({ status: "FAILED", message: "No Input" });
-  const { caller, limit, offset } = req.body;
-  if (!limit) limit = 15;
-  if (!offset) offset = 0;
-  if (!caller) {
-    return res.send({ status: "FAILED", message: "Invalid caller" });
+  try {
+    if (!req.body)
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.NO_INPUT });
+    const { caller, limit, offset } = req.body;
+    if (!limit) limit = 15;
+    if (!offset) offset = 0;
+    if (!caller) {
+      return res
+        .status(400)
+        .send({ status: STATUS.FAILED, message: MESSAGE.INVALID_INPUT });
+    }
+
+    let data = await ClaimEvent.find({ staker: caller });
+
+    let total = data.length;
+
+    // pagination
+    data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
+
+    // format result
+    const dataTable = data.map((data) => ({
+      staker: data.staker,
+      time: data.time,
+      reward_amount: data.reward_amount,
+    }));
+
+    return res.send({ status: STATUS.OK, ret: dataTable, total: total });
+  } catch (error) {
+    res.status(500).send({ status: STATUS.FAILED, message: error.message });
   }
-
-  let data = await ClaimEvent.find({ staker: caller });
-
-  let total = data.length;
-
-  // pagination
-  data = data.slice(parseInt(offset), parseInt(offset) + parseInt(limit));
-
-  // format result
-  const dataTable = data.map((data) => ({
-    staker: data.staker,
-    time: data.time,
-    reward_amount: data.reward_amount,
-  }));
-
-  return res.send({ status: "OK", ret: dataTable, total: total });
 };
